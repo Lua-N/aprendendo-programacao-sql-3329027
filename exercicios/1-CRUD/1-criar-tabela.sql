f9964dd0e1fdab623fbd7bf18389e3b66d0f6da9
Lua Nogueira <107567948+Lua-N@users.noreply.github.com>
2024-03-12 12:42:00 +0000

    new_table

--- exercicios/1-CRUD/1-criar-tabela.sql
+++ exercicios/1-CRUD/1-criar-tabela.sql
@@ -1,3 +1,5 @@
-CREAT TABLE tabela (
-coluna1 tipo_dado_coluna1,
-coluna2 tipo_dado_coluna2,...);
+CREATE TABLE salario_classe (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nivel TEXT,
+salario INTEGER
+);
